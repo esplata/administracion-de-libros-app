8c8c835eb4027f05c6fbb72e489eba8f234fbaa3
Cesare <es_plata@yahoo.it>
2026-03-23 12:12:13 +0100

    upd: fixed module

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -7,7 +7,7 @@ function Header() {
       <h1>Application de Administracion de Libros</h1>
       <hr />
       <div className="links">
-        <NavLink to="/" className="link" activeclassname="active" exact>
+        <NavLink to="/" className="link" activeclassname="active">
           Lista de Libros
         </NavLink>
         <NavLink to="/add" className="link" activeclassname="active">
