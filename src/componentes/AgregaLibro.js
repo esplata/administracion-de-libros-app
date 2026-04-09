2caca2128c867b135ccc4433da517cce02fadcfc
Cesare <es_plata@yahoo.it>
2026-04-09 11:12:52 +0200

    upd : component

--- src/componentes/AgregaLibro.js
+++ src/componentes/AgregaLibro.js
@@ -1,16 +1,22 @@
 import React from "react";
 import FormularioDeLibro from "./FormularioDeLibro";
+import Header from "./Header";
+const libros = require("../controller/libros");
 
 function AgregaLibro() {
   function handleOnSubmit(libro) {
     console.log(libro);
+
     console.log("Adesso dovresti scriverlo nel database Couch");
   }
 
   return (
-    <React.Fragment>
-      <FormularioDeLibro handleOnSubmit={handleOnSubmit} />
-    </React.Fragment>
+    <>
+      <Header />
+      <React.Fragment>
+        <FormularioDeLibro handleOnSubmit={handleOnSubmit} />
+      </React.Fragment>
+    </>
   );
 }
 
