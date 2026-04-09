98585581bf0d28bc7c846fa3eaa7ff051faa297e
Cesare <es_plata@yahoo.it>
2026-04-09 11:12:40 +0200

    upd: component

--- src/componentes/ListaDeLibros.js
+++ src/componentes/ListaDeLibros.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import Header from "./Header";
 
 function ListaDeLibros() {
-  return <h2>Lista de Libros</h2>;
+  return (
+    <>
+      <Header />
+      <h2>Lista de Libros</h2>
+      <hr />
+    </>
+  );
 }
 
 export default ListaDeLibros;
