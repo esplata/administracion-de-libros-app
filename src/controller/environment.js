a850206f91e5d3555ff68be071a1d67fb43aadee
Cesare <es_plata@yahoo.it>
2026-04-09 11:10:41 +0200

    add : environment definition

---
+++ src/controller/environment.js
@@ -0,0 +1,8 @@
+const dbAdmin = "admin";
+const dbAdminPwd = "casaspagna2013";
+const dbUrl = "192.168.15.21:5984";
+const dbUser = "testuser";
+const dbUserPwd = "testuser";
+const dbName = "testdb";
+
+module.exports = { dbAdmin, dbAdminPwd, dbUrl, dbUser, dbUserPwd, dbName };
