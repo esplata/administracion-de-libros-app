846587da7e42cce6f09ce853e2df106b9d1918d6
Cesare <es_plata@yahoo.it>
2026-04-09 11:10:03 +0200

    add: component Prinpial ...is the alternative to Body

---
+++ src/componentes/Principal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+export default function Principal() {
+  return (
+    <>
+      <Header />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
