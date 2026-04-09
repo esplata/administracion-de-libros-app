66f0eca4adc45dd1cf918a4034ccff8c729e9d39
Cesare <es_plata@yahoo.it>
2026-04-09 11:11:17 +0200

    add: main component

---
+++ src/router/main.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
+function Main() {
+  return (
+    <>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </>
+  );
+}
+export default Main;
+
+/*
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route element={<ListaDeLibros />} path="/" />
+            <Route element={<AgregaLibro />} path="/add" />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+
+*/
