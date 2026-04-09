ba82b8c27b70a8179a4dd02f65661d7666a1aabf
Cesare <es_plata@yahoo.it>
2026-04-09 11:10:20 +0200

    upd: db

--- src/controller/db.js
+++ src/controller/db.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
 const nano = require("nano");
-
-const dbAdmin = process.env.DB_ADMIN;
-const dbAdminPwd = process.env.DB_ADMIN_PASSWORD;
-const dbUrl = process.env.DB_URL;
-const dbUser = process.env.DB_USER;
-const dbUserPwd = process.env.DB_USER_PASSWORD;
-const dbName = process.env.DB_NAME;
+const {
+  dbAdmin,
+  dbAdminPwd,
+  dbUrl,
+  dbUser,
+  dbUserPwd,
+  dbName,
+} = require("./environment");
 
 const db = nano(
   `http://${dbAdmin}:${dbAdminPwd}@${dbUrl}` || "http://127.0.0.1:5984",
