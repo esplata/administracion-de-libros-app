0d164e057bedad1f32ccded77ff528905ac78e8c
Cesare <es_plata@yahoo.it>
2026-04-13 12:03:59 +0200

    upd fixed components

--- src/router/Router.js
+++ src/router/Router.js
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "form",
-        Component: FormTest,
+        Component: InsertLibro,
       },
       {
         path: "submit",
