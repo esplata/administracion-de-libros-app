dda09483499eb9da47471c6fe18b1f144c98c332
Cesare <es_plata@yahoo.it>
2026-04-13 19:15:22 +0200

    upd: change struncture

--- src/controller/db.js
+++ src/controller/db.js
@@ -1,24 +1,40 @@
 //const nano = require("nano");
-//require("dotenv").config();
+require("dotenv").config();
 
 const PouchDB = require("pouchdb").default;
 //import PouchDB from "pouchdb";
 
-const {
-  dbAdmin,
-  dbAdminPwd,
-  dbUrl,
-  dbUser,
-  dbUserPwd,
-  dbName,
-} = require("./environment");
+// const {
+//   dbAdmin,
+//   dbAdminPwd,
+//   dbUrl,
+//   dbUser,
+//   dbUserPwd,
+//   dbName,
+// } = require("./environment");
+
+const dbAdmin = process.env.DB_ADMIN;
+const dbAdminPwd = process.env.DB_ADMIN_PASSWORD;
+const dbUrl = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
 
 // const db = new pouchDB(
 //   `http://${process.env.DB_ADMIN}:${process.env.DB_ADMIN_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}`,
 // );
-const db = new PouchDB(
-  "http://admin:casaspagna2013@$192.168.15:21:5984/testdb",
-);
+
+// const db = new PouchDB("http://192.168.15.21:5984/books", {
+//   auth: {
+//     username: "admin",
+//     password: "casaspagna2013",
+//   },
+// });
+
+const db = new PouchDB(`http://${dbUrl}/${dbName}`, {
+  auth: {
+    username: `${dbAdmin}`,
+    password: `${dbAdminPwd}`,
+  },
+});
 
 // const db = nano(
 //   `http://${dbAdmin}:${dbAdminPwd}@${dbUrl}` || "http://127.0.0.1:5984",
