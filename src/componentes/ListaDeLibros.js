5868deb60230231f2c0106d60b91b3a5a0aafdc3
Cesare <es_plata@yahoo.it>
2026-04-14 12:42:41 +0200

    upd: terminate component for printing a list of books.

--- src/componentes/ListaDeLibros.js
+++ src/componentes/ListaDeLibros.js
@@ -1,16 +1,26 @@
-import React from "react";
-//import { Link, NavLink, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 import Header from "./Header";
+import Table from "./Table";
+const libros = require("../controller/libros");
 
-function ListaDeLibros() {
+export async function loader() {
+  const response = await libros.getAll();
+  const listaLibros = await response;
+  return listaLibros;
+}
+
+export default function ListaDeLibros() {
+  const listaLibri = useLoaderData();
+  const [libro, setLibro] = useState(listaLibri);
+
   //TODO: Bisogna creare il componente per leggere tutti i record dentro del database.
   return (
     <>
       <Header />
       <h2>Lista de Libros</h2>
+      <Table elencoLibri={libro} />
       <hr />
     </>
   );
 }
-
-export default ListaDeLibros;
